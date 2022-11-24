0e14ff0d8001f06e558709b1d0a940740b9b4af3
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2022-11-24 12:35:17 -0500

    clases

---
+++ src/ejercicios/08-clases-p2-constructor.ts
@@ -0,0 +1,29 @@
+/*
+    ===== Código de TypeScript =====
+*/
+/* Clases basicas */
+/* contructor de la clase */
+//class Heroe {
+  /* alterEgo: string;
+    edad: number;
+    nombreReal: string;
+
+    constructor(
+        alterEgo: string,
+        edad: number,
+        nombreReal: string
+    ) {
+        this.alterEgo = alterEgo;
+    }
+    // forma tradicional de definir un constructor
+    */
+
+  // forma simplificada de definir un constructor
+  /* constructor(
+    public alterEgo: string,
+    public edad: number,
+    public nombreReal: string
+  ) {} */
+//}
+/* const ironman = new Heroe("Ironman", 45, "Tony");
+console.log(ironman); */
