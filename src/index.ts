0ea0e877800d21b7404d0db89aef8c4f723df59a
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2022-11-24 12:38:14 -0500

    clases

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,4 @@
 /*
     ===== Código de TypeScript =====
 */
-/* Clases basicas */
-/* contructor de la clase */
-/* Extender clases */
-class PersonaNormal {
-    constructor(
-        public nombre: string,
-        public direccion: string
-    ) { }
-}
 
-class Heroe extends PersonaNormal {
-  constructor(
-    public alterEgo: string,
-    public edad: number,
-    public nombreReal: string
-  ) {
-    super(nombreReal, 'New York, USA');
-  }
-}
-const ironman = new Heroe("Ironman", 45, "Tony");
-console.log(ironman);
